7649e562beb46ae150ab28b986aade023ce8295f
Jessica Stokes <hello@jessicastokes.net>
2019-06-23 23:03:52 -0700

    Initial work on parsing uploaded data

--- dreamuploader/__init__.py
+++ dreamuploader/__init__.py
@@ -64,3 +64,85 @@ class DreamuploaderRequest(server.Request):
 
         # finally, hand it on back to the original Request class
         super(DreamuploaderRequest, self).requestReceived(command, path, version)
+
+############ TODO: Split this into a separate file bc this is no way to live ############
+
+import attr
+from base64 import b64decode
+from datetime import datetime
+from urllib.parse import parse_qs
+
+# Hey Sega, uhhhh, what the heck is this about?
+DREAMCAST_BASE64_TRANSLATION = bytes.maketrans(
+    b'AZOLYNdnETmP6ci3Sze9IyXBhDgfQq7l5batM4rpKJj8CusxRF+k2V0wUGo1vWH/=',
+    b'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+)
+
+class VMSData:
+    """
+    A representation of Dreamcast Visual Memory System data,
+    including both the raw file and file-system metadata.
+    """
+
+    # Raw Visual Memory file data
+    vms = None
+
+    # Metadata for VMI sidecar file
+    filename = None
+    filesize = None
+    blocksize = None
+    tp = None # TODO: oWo What is this? 🤔
+    fl = None # TODO: oWo What is this? 🤔
+    of = None # TODO: oWo What is this? 🤔
+    timestamp = None
+
+    def __init__(self, inbytes):
+
+        bytes = BytesIO(inbytes)
+        header = BytesIO()
+
+        nextLine = bytes.readline()
+
+        while nextLine:
+            strippedLine = nextLine.strip()
+
+            if strippedLine is not b'':
+                header.write(strippedLine)
+            else:
+                break
+
+            nextLine = bytes.readline()
+
+        header.seek(0)
+        metadata = parse_qs(header.read())
+        header.close()
+
+        filename = metadata[b'filename']
+        self.filename = filename[0].decode()
+
+        filesize = metadata[b'fs']
+        self.filesize = int(filesize[0])
+
+        blocksize = metadata[b'bl']
+        self.blocksize = int(blocksize[0])
+
+        tp = metadata[b'tp']
+        self.tp = tp[0]
+
+        fl = metadata[b'fl']
+        self.fl = fl[0]
+
+        of = metadata[b'of']
+        self.of = of[0]
+
+        timestamp = metadata[b'tm']
+        self.timestamp = datetime.strptime(timestamp[0].decode(), '%Y%m%d%H%M%S%w')
+
+        body = bytes.read()
+
+        self.vms = b64decode(body.translate(DREAMCAST_BASE64_TRANSLATION))
+        bytes.close()
+
+    # @classmethod
+    # def from_path(cls, path):
+
---
+++ dreamuploader/test/test_vmifile.py
@@ -0,0 +1,36 @@
+import unittest
+from datetime import datetime
+import os
+
+from dreamuploader import VMSData
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def read_fixture(name):
+    return open(os.path.join(dir_path, 'fixtures', name), 'rb').read()
+
+CAPVSSNK_PLANETWEB = read_fixture('CAPVSSNK_SYS-Planetweb.txt')
+CAPVSSNK_DREAMPASSPORT = read_fixture('CAPVSSNK_SYS-DreamPassport.txt')
+CAPVSSNK_VMS = read_fixture('CAPVSSNK_SYS.VMS')
+
+class VMSDataTests(unittest.TestCase):
+
+    def test_planetwebFormatInput(self):
+
+        # Planetweb uses Windows-style line endings
+        parsed = VMSData(CAPVSSNK_PLANETWEB)
+        self.assertEqual(parsed.vms, CAPVSSNK_VMS)
+        self.assertEqual(parsed.filename, 'CAPVSSNK_SYS')
+        self.assertEqual(parsed.filesize, 4608)
+        self.assertEqual(parsed.blocksize, 9)
+        self.assertEqual(parsed.timestamp, datetime.fromisoformat('2019-06-22T22:51:40'))
+
+    def test_dreamPassportFormatInput(self):
+
+        # Dream Passport uses Unix-style line endings
+        parsed = VMSData(CAPVSSNK_DREAMPASSPORT)
+        self.assertEqual(parsed.vms, CAPVSSNK_VMS)
+        self.assertEqual(parsed.filename, 'CAPVSSNK_SYS')
+        self.assertEqual(parsed.filesize, 4608)
+        self.assertEqual(parsed.blocksize, 9)
+        self.assertEqual(parsed.timestamp, datetime.fromisoformat('2019-06-22T22:51:40'))
