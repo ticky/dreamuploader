42f0e1313e410ae5c09a493c78d89ca5aa944b86
Jessica Stokes <hello@jessicastokes.net>
2019-11-21 23:35:26 -0800

    Add an extra spec for VMS data to ensure checksum is valid

--- dreamuploader/test/test_vmsdata.py
+++ dreamuploader/test/test_vmsdata.py
@@ -10,13 +10,18 @@ def read_fixture(name):
     return open(os.path.join(dir_path, 'fixtures', name), 'rb').read()
 
 CAPVSSNK_PLANETWEB = read_fixture('CAPVSSNK_SYS-Planetweb.txt')
+
 CAPVSSNK_DREAMPASSPORT = read_fixture('CAPVSSNK_SYS-DreamPassport.txt')
-CAPVSSNK_VMS = read_fixture('CAPVSSNK_SYS.VMS')
+CAPVSSNK_VMS = read_fixture('CAPVSSNK.VMS')
 CAPVSSNK_VMI = read_fixture('CAPVSSNK.VMI')
+
+PYOFEVER_DREAMPASSPORT = read_fixture('PUYOFEVERSYS-DreamPassport.txt')
+PYOFEVER_VMS = read_fixture('PYOFEVER.VMS')
+PYOFEVER_VMI = read_fixture('PYOFEVER.VMI')
 
 class VMSDataTests(unittest.TestCase):
 
-    def test_planetwebFormatPOSTInput(self):
+    def test_planetwebFormatCAPVSSNKPOSTInput(self):
 
         # Planetweb uses Windows-style line endings
         parsed = VMSData.from_post_bytes(CAPVSSNK_PLANETWEB)
@@ -26,7 +31,7 @@ class VMSDataTests(unittest.TestCase):
         self.assertEqual(parsed.timestamp, datetime.fromisoformat('2019-06-22T22:51:40'))
         self.assertEqual(parsed.to_vmi('CAPVSSNK'), CAPVSSNK_VMI)
 
-    def test_dreamPassportFormatPOSTInput(self):
+    def test_dreamPassportFormatCAPVSSNKPOSTInput(self):
 
         # Dream Passport uses Unix-style line endings
         parsed = VMSData.from_post_bytes(CAPVSSNK_DREAMPASSPORT)
@@ -35,6 +40,15 @@ class VMSDataTests(unittest.TestCase):
         self.assertEqual(parsed.filesize, 4608)
         self.assertEqual(parsed.timestamp, datetime.fromisoformat('2019-06-22T22:51:40'))
         self.assertEqual(parsed.to_vmi('CAPVSSNK'), CAPVSSNK_VMI)
+
+    def test_dreamPassportFormatPUYOFEVERSYSPOSTInput(self):
+
+        parsed = VMSData.from_post_bytes(PYOFEVER_DREAMPASSPORT)
+        self.assertEqual(parsed.vms, PYOFEVER_VMS)
+        self.assertEqual(parsed.filename, 'PUYOFEVERSYS')
+        self.assertEqual(parsed.filesize, 2048)
+        self.assertEqual(parsed.timestamp, datetime.fromisoformat('2005-08-28T23:27:24'))
+        self.assertEqual(parsed.to_vmi('PYOFEVER'), PYOFEVER_VMI)
 
     def test_fromVMI(self):
 
